dbc998393d7208ac7c84dfd198f6b515280aa960
shubh8101996 <shubhamshedge8@gmail.com>
2023-03-01 20:27:06 +0530

    second commit by shubham

---
+++ src/main/java/com/orm/utility/PropertiesRead.java
@@ -0,0 +1,56 @@
+package com.orm.utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesRead {
+
+	Properties prop;
+
+	public PropertiesRead() {
+
+		String path = "C:\\Users\\shubh\\new workspace\\FrameworkOrangeHRM\\TestDataFile\\env1config.properties";
+		prop = new Properties();
+
+		try {
+			FileInputStream file = new FileInputStream(path);
+			prop.load(file);
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public String URL() {
+
+		String url = prop.getProperty("url");
+
+		return url;
+	}
+
+	public String Username() {
+
+		String username = prop.getProperty("username");
+
+		return username;
+	}
+
+	public String Password() {
+
+		String password = prop.getProperty("password");
+
+		return password;
+	}
+
+	public String Browser() {
+
+		String browserval = prop.getProperty("browser");
+
+		return browserval;
+	}
+
+}
