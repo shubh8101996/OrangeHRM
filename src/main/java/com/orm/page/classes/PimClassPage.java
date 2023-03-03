b659163ccc473ff81b843684b4ef7aaade9afef8
yogesh6303 <newadkaryogesh@gmail.com>
2023-03-03 16:22:53 +0530

    Yogesh first commited

--- src/main/java/com/orm/page/classes/PimClassPage.java
+++ src/main/java/com/orm/page/classes/PimClassPage.java
@@ -1,10 +1,33 @@
 package com.orm.page.classes;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PimClassPage {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	@FindBy(xpath = "/html/body/div/div[1]/div[2]/div[2]/div/div[2]/div[1]/button")
+	private WebElement addbtn;
+	
+	@FindBy(name = "firstName")
+	private WebElement firstName;
+	
+	@FindBy(name = "middleName")
+	private WebElement middleName;
+	
+	@FindBy(name = "lastName")
+	private WebElement lastName;
+	
+	@FindBy(xpath = "(//input[@class='oxd-input oxd-input--active'])[2]")
+	private WebElement id;
+	
+	@FindBy(xpath = "/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]")
+	private WebElement savebtn;
+	
+	@FindBy(xpath = "/html/body/div[1]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[1]")
+	private WebElement cancelbtn;
+	
+	@FindBy(xpath = "/html/body/div/div[1]/div[2]/div[2]/div/div/form/div[1]/div[1]/div/div[2]/div/button/i")
+	private WebElement addphoto;
+	
 
 }
